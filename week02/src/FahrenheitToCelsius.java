eeb8b826315a6246612a1d592f50b31b6abc5ea0
ag4710 <leun4710@gmail.com>
2023-09-05 19:14:02 +0900

    week02

---
+++ week02/src/FahrenheitToCelsius.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // (100°F − 32) × 5/9 = 37.778°C
+        double fahrenheit = sc.nextDouble()
+    }
+}
