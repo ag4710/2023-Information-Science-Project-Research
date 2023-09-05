0289784c09a4ac3bebd3851f45420fe57c09095d
ag4710 <leun4710@gmail.com>
2023-09-05 22:48:35 +0900

    week01 final

---
+++ week01/src/Hello.java
@@ -0,0 +1,9 @@
+// alt + shift + F10 프로그램 실행 단축
+public class Hello {
+    // psvm + Enter
+    public static void main(String[] args) {
+        System.out.println("안녕 세계~");
+        System.out.print("꿈두레 인천과학고 ");
+        System.out.println("초은고 신현고 제물포고");  // sout + Enter
+    }
+}
