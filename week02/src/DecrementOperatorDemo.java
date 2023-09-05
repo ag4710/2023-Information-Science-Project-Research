8880b1eeffcfc7cfc1cba06363fb157043e79eec
ag4710 <leun4710@gmail.com>
2023-09-05 22:45:02 +0900

    test 6

--- week02/src/DecrementOperatorDemo.java
+++
@@ -1,9 +0,0 @@
-public class DecrementOperatorDemo {
-    public static void main(String[] args) {
-        int a = 9, b =7;
-
-        System.out.println(b--);
-        System.out.println(--b - a--);
-        System.out.println(b + a);
-    }
-}
