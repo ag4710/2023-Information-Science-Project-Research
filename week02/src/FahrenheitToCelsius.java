ec19a7ef0524d9c44b24660f3a9e7e94f5bee564
ag4710 <leun4710@gmail.com>
2023-09-05 19:25:44 +0900

    week02 FahrenheitToCelsius v0.2

--- week02/src/FahrenheitToCelsius.java
+++ week02/src/FahrenheitToCelsius.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class FahrenheitToCelsius {
     public static void main(String[] args) {
+        double fahrenheit, celsius;
         Scanner sc = new Scanner(System.in);
         // (100°F − 32) × 5/9 = 37.778°C
-        double fahrenheit = sc.nextDouble();
-        double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+//        double fahrenheit = sc.nextDouble();
+//        double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+        fahrenheit = sc.nextDouble();
+        celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
         System.out.println("화씨 " + fahrenheit + "도는 섭씨 " + celsius + "도 입니다");
     }
 }
