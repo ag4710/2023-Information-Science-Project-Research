367bc8b8e346a9c2f58527f97901418bd9d08075
ag4710 <leun4710@gmail.com>
2023-09-12 21:27:34 +0900

    week03 GuessGame v0.2

--- week03/src/GuessGame.java
+++ week03/src/GuessGame.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class GuessGame {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            int answer = (int) (Math.random() * 100) + 1;
-            System.out.println(answer);
+        Scanner scanner = new Scanner(System.in);
+        int answer = (int) (Math.random() * 100) + 1;
+        System.out.println(answer);
+        System.out.print("1에서 100사이의 수 입력 : ");
+        int guessNumber = scanner.nextInt();
+        if(guessNumber == answer){
+            System.out.println("정답!");
+        }else{
+            System.out.println("정답은 " + answer + "입니다!");
         }
     }
 }
