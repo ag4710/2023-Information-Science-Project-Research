e966e57b8f2ab9fc6529611136d45543b75818e9
ag4710 <leun4710@gmail.com>
2023-09-12 20:45:38 +0900

    week03 DiceDemo v0.2

--- week03/src/DiceDemo.java
+++ week03/src/DiceDemo.java
@@ -1,5 +1,20 @@
 public class DiceDemo {
     public static void main(String[] args) {
-        System.out.println((int)(Math.random()*6)+1);
+        int dice = (int)(Math.random()*6)+1;
+        if(dice == 1){
+            System.out.println("1!");
+        } else if(dice == 2){
+            System.out.println("2!");
+         }else if(dice == 3){
+            System.out.println("3!");
+        } else if(dice == 4){
+            System.out.println("4!");
+        } else if(dice == 5){
+            System.out.println("5!");
+        } else if(dice == 6){
+            System.out.println("6!");
+        } else{
+            System.out.println("여긴 영원히 실행되지 않습니다!");
+        }
     }
 }
