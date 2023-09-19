14c07eee44f40c05dfffb813899f6258a12b8135
ag4710 <leun4710@gmail.com>
2023-09-19 22:23:24 +0900

    week04 PrimeNumber v1.1

--- week04/src/PrimeNumber.java
+++ week04/src/PrimeNumber.java
@@ -4,20 +4,21 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Input number : ");
+
         int number = s.nextInt();
+        boolean isPrime = true;  // int -> boolean 메모리 공간 절약, 가독성 up
 
-        int count = 0;
-
-        for(int i=2; i<number; i++){  // 2부터 입력된 수까지 반복 (기존 코드대비 반복 횟수 -2)
-            if(number % i == 0) {  // 약수가 발생하면
-                count = count + 1;  // 약수 카운터 증가
-                // System.out.println(i);
+        for(int i=2; i<number; i++){
+            if(number % i == 0) {
+                //count = count + 1;
+                isPrime = false;  // 더하기 연산 제거
             }
         }
 
-        if(count == 0)  // 한번 나누어 떨어지지 않았다면 count는 0으로 유지됨
+        if(isPrime) {  // == 비교연산자 제거
             System.out.println(number + " is prime number!");
-        else
+        }else {
             System.out.println(number + " is not prime number!");
+        }
     }
 }
