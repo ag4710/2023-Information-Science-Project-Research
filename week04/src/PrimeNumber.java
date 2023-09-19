a24d0d30a776c520b19a2f663a91970ee2427a14
ag4710 <leun4710@gmail.com>
2023-09-19 22:29:16 +0900

    week04 PrimeNumber v2.1

--- week04/src/PrimeNumber.java
+++ week04/src/PrimeNumber.java
@@ -4,24 +4,25 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Input number : ");
+        int number1 = s.nextInt();
+        System.out.print("Input number : ");
+        int number2 = s.nextInt();
 
-        int number = s.nextInt();
         boolean isPrime = true;
 
-        if(number <= 1)  // 소수 정의 참고
-            isPrime = false;
-        else{
-            for(int i=2; i<number; i++){
-                if(number % i == 0) {
-                    isPrime = false;
-                    break;
+        for(int k=number1; k<=number2; k++) {  // 입력된 두 수 사이의 모든 수를 체크
+            isPrime = true;  // 각 수를 체크할 때 isPrime 변수를 true로 초기화한다.
+            if(k <= 1)  // 소수 정의 참고
+                isPrime = false;
+            else {
+                for (int i = 2; i < k; i++) {
+                    if (k % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
+                if(isPrime) System.out.print(k + " ");
             }
         }
-
-        if(isPrime)
-            System.out.println(number + " is prime number!");
-        else
-            System.out.println(number + " is not prime number!");
     }
 }
