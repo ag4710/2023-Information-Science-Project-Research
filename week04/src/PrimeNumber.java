569cab3080b75e4d81f6d84bb67958fa3d3e8bc2
ag4710 <leun4710@gmail.com>
2023-09-19 22:30:58 +0900

    week04 PrimeNumber v2.2

--- week04/src/PrimeNumber.java
+++ week04/src/PrimeNumber.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+// 두 수를 입력받아 두 수를 포함한 사이의 소수를 출력하라.
 
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Input number : ");
+        System.out.print("Input start number : ");
         int number1 = s.nextInt();
-        System.out.print("Input number : ");
+        System.out.print("Input end number : ");
         int number2 = s.nextInt();
 
         boolean isPrime = true;
+
+        if(number1 > number2){  // 먼저 입력된 수가 클 경우 두 수 swqp
+            int temp;
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
 
         for(int k=number1; k<=number2; k++) {  // 입력된 두 수 사이의 모든 수를 체크
             isPrime = true;  // 각 수를 체크할 때 isPrime 변수를 true로 초기화한다.
