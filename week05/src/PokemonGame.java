543c64fe56e8568b7dc808f42d1273ab28d16dd1
ag4710 <leun4710@gmail.com>
2023-10-10 19:25:45 +0900

    week05 PokemonGame v0.2

--- week05/src/PokemonGame.java
+++ week05/src/PokemonGame.java
@@ -1,10 +1,16 @@
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon pikachu = new Pokemon();
-        pikachu.hp = 100;
-        pikachu.name = "피카츄";
-        pikachu.attack();
-        System.out.println(pikachu.name);
-        System.out.println(pikachu.hp);
+        Pokemon pikachu1 = new Pokemon();
+        Pokemon pikachu2 = new Pokemon();
+        Pokemon squirtle1 = new Pokemon();
+        squirtle1.name = "꼬부기";
+        squirtle1.hp = 110;
+
+        pikachu2.attack();
+        pikachu1.hp = 100;
+        pikachu1.name = "피카츄";
+        pikachu1.attack();
+        System.out.println(pikachu1.name);
+        System.out.println(pikachu1.hp);
     }
 }
