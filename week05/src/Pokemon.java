c14ae94a027e35aaacda3c59437dd0afd9c88ae8
ag4710 <leun4710@gmail.com>
2023-10-10 19:39:01 +0900

    week05 PokemonGame v0.3

--- week05/src/Pokemon.java
+++ week05/src/Pokemon.java
@@ -4,8 +4,12 @@ public class Pokemon {
     // fields (attributes)
     public int hp;
     public String name;
-    // methods (member function)
-    public void attack(){
-        System.out.println("공격시전!");
+    // constructor method
+    public  Pokemon(){
+        hp = 100;
+        name = "피카츄";
+        System.out.println("피카피카~");
     }
+    // methods (member function)
+    public void attack(){ System.out.println("공격시전!"); }
 }
--- week05/src/PokemonGame.java
+++ week05/src/PokemonGame.java
@@ -3,13 +3,16 @@ public class PokemonGame {
         Pokemon pikachu1 = new Pokemon();
         Pokemon pikachu2 = new Pokemon();
         Pokemon squirtle1 = new Pokemon();
-        squirtle1.name = "꼬부기";
-        squirtle1.hp = 110;
+//        squirtle1.name = "꼬부기";
+//        squirtle1.hp = 110;
 
-        pikachu2.attack();
-        pikachu1.hp = 100;
-        pikachu1.name = "피카츄";
-        pikachu1.attack();
+//        pikachu2.attack();
+//        pikachu1.hp = 100;
+//        pikachu1.name = "피카츄";
+//        pikachu1.attack();
+
+        System.out.println(squirtle1.name);
+        System.out.println(squirtle1.hp);
         System.out.println(pikachu1.name);
         System.out.println(pikachu1.hp);
     }
