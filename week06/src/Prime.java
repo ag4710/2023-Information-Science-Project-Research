8271183faa23e9adcf1f8278b25ff34e92855cef
ag4710 <leun4710@gmail.com>
2023-10-17 19:55:56 +0900

    week06 PrimeNumber v3.2

--- week06/src/Prime.java
+++ week06/src/Prime.java
@@ -1,35 +1,15 @@
-import java.util.Scanner;
-
 public class Prime {
     public Prime() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Input start number : ");
-        int number1 = s.nextInt();
-        System.out.print("Input end number : ");
-        int number2 = s.nextInt();
 
-        boolean isPrime = true;
+    }
 
-        if(number1 > number2){  // 먼저 입력된 수가 클 경우 두 수 swqp
-            int temp;
-            temp = number1;
-            number1 = number2;
-            number2 = temp;
+    public boolean isPrime(int n){          //method 이름으로 사용
+        if(n <= 1)  // 소수 정의 참고
+            return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
         }
-
-        for(int k=number1; k<=number2; k++) {  // 입력된 두 수 사이의 모든 수를 체크
-            isPrime = true;  // 각 수를 체크할 때 isPrime 변수를 true로 초기화한다.
-            if(k <= 1)  // 소수 정의 참고
-                isPrime = false;
-            else {
-                for (int i = 2; i < k; i++) {
-                    if (k % i == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-                if(isPrime) System.out.print(k + " ");
-            }
-        }
+        return true;
     }
 }
--- week06/src/PrimeNumber.java
+++ week06/src/PrimeNumber.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args) {
         Prime p1 = new Prime();
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input start number : ");
+        int number1 = s.nextInt();
+        System.out.print("Input end number : ");
+        int number2 = s.nextInt();
+
+        if(number1 > number2){  // 먼저 입력된 수가 클 경우 두 수 swqp
+            int temp;
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+
+        for(int k=number1; k<=number2; k++) {  // 입력된 두 수 사이의 모든 수를 체크
+            if(p1.isPrime(k)) System.out.print(k + " ");
+        }
     }
 }
