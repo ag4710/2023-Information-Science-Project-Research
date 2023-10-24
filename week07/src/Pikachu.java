631f0cf4f89b956eb8b75916492f72af664efdab
ag4710 <leun4710@gmail.com>
2023-10-24 20:21:16 +0900

    week07 PokemonGame v1.1

--- week07/src/Pikachu.java
+++ week07/src/Pikachu.java
@@ -7,8 +7,9 @@ public class Pikachu extends Pokemon{ // Pikachu is a Pokemon, Inheritance
     }
 
     public Pikachu(int hp, String name) {  // 가독성을 위해 매개변수 이름을 name으로
-        this.name = name;  // 매개변수와 멤버변수(필드)를 구분하기 위해 this 키워드 사용
-        this.hp = hp;
+//        this.name = name;  // 매개변수와 멤버변수(필드)를 구분하기 위해 this 키워드 사용
+//        this.hp = hp;
+        super(hp, name);  // 부모 클래스(Pokemon)의 매개변수 생성자를 호출
         System.out.println("라이츄라이츄~");
     }
 
